880065e3ef6780f09bab2bf700ca26e44cc05268
Reilly Edwards <reiman2222@gmail.com>
2015-01-07 23:19:16 -0500

    Completed code

--- Mecanum_Interative/src/org/usfirst/frc/team2642/robot/Robot.java
+++ Mecanum_Interative/src/org/usfirst/frc/team2642/robot/Robot.java
@@ -6,7 +6,6 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.Relay;
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -19,40 +18,38 @@ public class Robot extends IterativeRobot {
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-	
 	RobotDrive drive;
     Joystick joystick1;
-    
     
     final int frontLeftChannel	= 2;
     final int rearLeftChannel	= 3;
     final int frontRightChannel	= 1;
     final int rearRightChannel	= 0;
+    final int joystick1Channel = 0;
     
     public void robotInit() {
+    	drive.setExpiration(0.1);
     	drive = new RobotDrive(frontLeftChannel, rearLeftChannel, frontRightChannel, rearRightChannel);
+    	drive.setInvertedMotor(MotorType.kFrontLeft, true);	// invert the left side motors
+    	drive.setInvertedMotor(MotorType.kRearLeft, true);	
+    	joystick1 = new Joystick(joystick1Channel);
     }
-
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-
+    	drive.setSafetyEnabled(false);
     }
-
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
         while(isOperatorControl() && isEnabled()){
+        	drive.setSafetyEnabled(true);
         	drive.mecanumDrive_Cartesian(joystick1.getX(), joystick1.getY(), joystick1.getZ(), 0);
         }
+        Timer.delay(0.005);	
     }
-    
-    
-    
-    
-    
     /**
      * This function is called periodically during test mode
      */
